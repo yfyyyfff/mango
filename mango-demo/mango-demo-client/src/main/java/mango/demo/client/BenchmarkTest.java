e891ff8ed36ae6ce98702b9098444b744f59fe2b
yangfan <yfyyyfff@163.com>
2018-05-08 18:24:27 +0800

    改了几个配置文件

--- mango-demo/mango-demo-client/src/main/java/mango/demo/client/BenchmarkTest.java
+++ mango-demo/mango-demo-client/src/main/java/mango/demo/client/BenchmarkTest.java
@@ -1,12 +1,32 @@
 package mango.demo.client;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import mango.demo.service.DemoService;
+
 /**
  * @author Ricky Fung
  */
 public class BenchmarkTest {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:mango-client.xml");
 
-    }
+		DemoService service = (DemoService) ctx.getBean("demoService");
+
+		service.hello("rpc");
+		System.out.println("echo:" + service.echo("rpc"));
+
+		List<String> hobbies = new ArrayList<>();
+		hobbies.add("NBA");
+		hobbies.add("Reading");
+		Map<String, String> map = service.introduce("hh", hobbies);
+		System.out.println("map:" + map);
+	}
 }
